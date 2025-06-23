b98fded5257ec8860f65fa8582e1f67dccf60624
deltaZenith <217093610+deltaZenith@users.noreply.github.com>
2025-06-23 13:23:42 +0200

    Added dnf support and logging capabilities

--- README.md
+++ README.md
@@ -5,7 +5,7 @@ DAPU (Distro Agnostic Package Utility) aims to be a simple, no frills, cross-com
 This script is meant for users who do not wish to learn their distro's package manager's syntax and commands, this software's main goal is to facilitate package management by making it automated and interactive. While the script doesn't include any code from other projects, it wraps around the package manager which is detected automatically, therefore the script has your distro's package manager as a dependency alongside other Python libraries and the Python programming language itself.
 
 ### Cross compatibility
-At the moment, the only supported package managers are APT and pacman, with more to come. The script figures out what package manager is installed using the shutils library and determines the approriate command for each situation through the use of dictionaries.
+At the moment, the supported package managers are APT, dnf and pacman, with more to come. The script figures out what package manager is installed using the shutils library and determines the approriate command for each situation through the use of dictionaries.
 
 ### Automation
 The process of managing packages is entirely automatic, the only thing the user has to do is provide the information required by DAPU to know what to do and what package to interact with (e.g. when installing or removing a package, the user will be prompted to specify its name and to confirm the operation.)
@@ -14,11 +14,30 @@ The process of managing packages is entirely automatic, the only thing the user 
 The script, upon launching, checks for elevated privileges and prints a list of options, each of which corresponds to a number, to know what to do, the script relies on user input which is expected to be one of the aforementioned numbers.
 
 ## Installation & Usage
+### Dependecies
+Python, which is preinstalled on most systems by default;  
+Your distro's package manager which needs to be among the supported ones (APT, dnf, pacman.)
 ### Clone the repo
-```git clone https://github.com/deltaZenith/DAPU.git```
+This will download the files in the repository to your current directory:
+``git clone https://github.com/deltaZenith/DAPU.git``
+### CD into the DAPU folder
+From the directory in which you cloned the repository, this command will change your working directory to DAPU's folder:
+``cd DAPU``  
 ### Run the script
-```cd DAPU```  
-```sudo python3 dapu.py```
+Either run it with the python command directly:  
+``sudo python3 dapu.py``  
+or make it executable and run it:  
+`` chmod +x dapu.py``   
+``sudo ./dapu.py``
+### Checking the logs
+Upon running the script, a directory containing DAPU's logs will be created in /var/log/dapu/ .  
+To check the logs, run:  
+``cat /var/log/dapu/dapu.log``
+This will return the file's content which represent every command that has been run.
+
+## To do list
+Add zypper supported  
+Add advanced features for each of the package managers
 
 ## Disclaimer
 This software relies on pacman, the package manager for Arch Linux; APT and apt-get, the package managers for Debian; the Python programming language and the "subprocess", the "shutils" and "os" Python libraries.
@@ -28,4 +47,4 @@ By using this script you agree that the author is not responsible for any damage
 Copyright (C) 2025 deltaZenith.  
 This file is part of DAPU.  
 DAPU is free software: you can redistribute it and/or modify it under the terms of the GNU General Public License as published by the Free Software Foundation, either version 3 of the License, or (at your option) any later version.  
-This program is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU General Public License for more details. You should have received a copy of the GNU General Public License along with this program. If not, see <http://www.gnu.org/licenses/> .
+This program is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU General Public License for more details. You should have received a copy of the GNU General Public License along with this program. If not, see <https://www.gnu.org/licenses/> .
--- dapu.py
+++ dapu.py
@@ -1,25 +1,43 @@
-#! /usr/bin/env python3
+#!/usr/bin/env python3
 # Copyright (C) 2025 deltaZenith
 # This file is part of DAPU
-# DAPU is free software: you can redistribute it and/or modify it under the terms of the GNU General Public License as published by the Free Software Foundation, either version 3 of the License, or (at your option) any later version. This program is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU General Public License for more details. You should have received a copy of the GNU General Public License along with this program. If not, see <http://www.gnu.org/licenses/>.
+# DAPU is free software: you can redistribute it and/or modify it under the terms of the GNU General Public License as published by the Free Software Foundation, either version 3 of the License, or (at your option) any later version. This program is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU General Public License for more details. You should have received a copy of the GNU General Public License along with this program. If not, see <https://www.gnu.org/licenses/>.
 
 import subprocess
 import shutil
 import os
+import logging
+
+os.makedirs("/var/log/dapu", exist_ok=True)
 
 pm=""
-supported_pms=["pacman", "apt"]
+supported_pms=["pacman", "apt", "dnf"]
 options=["Install packages", "Update the system", "Remove a package", "List installed packages", "Search for a package in the repos", "Clear the package cache", "Exit"]
+logging.basicConfig(filename="/var/log/dapu/dapu.log", format='%(asctime)s %(levelname)s:%(message)s', level=logging.DEBUG)
 commands={
         "pacman":{"install":["pacman", "-S", "--noconfirm"], "update":["pacman", "-Syu", "---noconfirm"], "remove":["pacman", "-Rns", "--noconfirm"], "query":["pacman", "-Q"], "search":["pacman", "-Ss"], "cache":["pacman", "-Sc", "--noconfirm"]},
-        "apt-get":{"install":["apt-get", "install", "-y"], "update":["apt-get", "update"], "upgrade":["apt-get", "upgrade", "-y"] ,"remove":["apt-get", "purge", "-y"], "query":["apt", "list", "--installed"], "search": ["apt", "search"], "cache":["apt-get", "autoclean", "-y"]}
+        "apt-get":{"install":["apt-get", "install", "-y"], "update":["apt-get", "update"], "upgrade":["apt-get", "upgrade", "-y"] ,"remove":["apt-get", "purge", "-y"], "query":["apt", "list", "--installed"], "search": ["apt", "search"], "cache":["apt-get", "autoclean", "-y"]},
+        "dnf":{"install":["dnf", "install", "-y"], "update":["dnf", "upgrade", "-y"], "remove":["dnf", "remove", "-y"], "query":["dnf", "list", "--installed"], "search":["dnf", "search"], "cache":["dnf", "clean", "all"]}
         }
 
-def check_uid():
+def check_euid():
     if os.geteuid()==0:
         pass
     else:
         exit("You need elevated (sudo) privileges to run this program.")
+
+def check_pm():
+    global pm
+    if shutil.which("pacman"):
+        pm="pacman"
+    elif shutil.which("apt-get"):
+        pm="apt-get"
+    elif shutil.which("dnf"):
+        pm="dnf"
+    else:
+        subprocess.run(["clear"])
+        exit(f"Your package manager isn't supported. The supported package managers are: supported_pms")
+    logging.info(f" Detected package manager: {pm}")
 
 def list_options():  
     print("=================DAPU==================")
@@ -28,20 +46,14 @@ def list_options():
     print("=======================================")
 def check_command():
     command=input(f"What do you want to do? (1-{str(len(options))}) ")
-    print("=======================================")
-    if shutil.which("pacman"):
-        pm="pacman"
-    elif shutil.which("apt-get"):
-        pm="apt-get"
-    else:
-        subprocess.run(["clear"])
-        exit(f"Your package manager isn't supported. The supported package managers are: supported_pms")
+    print("=======================================") 
     if command=="1":
         pkg=input("Enter the name of the package to install: ").lower()
         confirm = input(f"The package '{pkg}' will be installed, do you wish to continue? [y/n] ")
         if confirm == "y":
             print("Installing", pkg,"...")
             subprocess.run(commands[pm]["install"]+[pkg])
+            logging.info(f" Ran: {' '.join(commands[pm]['install'] + [pkg])}")
             print("==========INSTALLATION COMPLETED==========")
         else:
             pass
@@ -50,8 +62,10 @@ def check_command():
         if confirm == "y":
             print("Updating the system...")
             subprocess.run(commands[pm]["update"])
-            if pm != "pacman":
+            logging.info(f" Ran: {' '.join(commands[pm]['update'])}")
+            if pm == "apt-get":
                 subprocess.run(commands[pm]["upgrade"])
+                logging.info(f" Ran: {' '.join(commands[pm]['upgrade'])}")
             print("========UPDATE COMPLETED========")
         else:
             pass
@@ -62,8 +76,10 @@ def check_command():
             if confirm == "y":
                 print("Removing", pkg, "...")
                 subprocess.run(commands[pm]["remove"]+[pkg])
+                logging.info(f" Ran: {' '.join(commands[pm]['remove'] + [pkg])}")
                 if pm == "apt-get":
                     subprocess.run(["apt-get", "autoremove", "--purge", "-y"])
+                    logging.info(f" Ran: {' '.join('apt-get autoremove --purge -y')}")
                 print("===========REMOVAL COMPLETED===========")
             else:
                 pass
@@ -76,23 +92,27 @@ def check_command():
     elif command=="5":
         pkg=input("Enter the name of the package to search for: ").lower()
         output = subprocess.run(commands[pm]["search"] + [pkg], capture_output=True, text=True)
+        logging.info(f" Ran: {' '.join(commands[pm]['search'] + [pkg])}")
         print("================RESULTS================")
         print(output.stdout)
     elif command=="6":
-        print("Pulisco la cache...")
         confirm = input("The cache will be cleaned, do you wish to continue? [y/n] ")
+        print("Cleaning cache...")
         if confirm == "y":
             subprocess.run(commands[pm]["cache"])
+            logging.info(f" Ran: {' '.join(commands[pm]['cache'])}")
             print("==============CACHE CLEANED==============")
         else:
             pass
     elif command ==(str(len(options))):
+        logging.info(f"{(' Exited.')}")
         exit("Exited successfully")
     else:
         print("Invalid option.")
         pass
 
-check_uid()
+check_euid()
+check_pm()
 while True:
     list_options()
     check_command()
